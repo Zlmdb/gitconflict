f413795a66f57c4b37e1da0297da641a4a6ff169
Tony <htcm8w@163.com>
2018-05-30 11:37:35 +0800

    modified dev.js and work.html

--- dev.js
+++ dev.js
@@ -1,1 +1,23 @@
-console.log('dev')
+console.log('dv发给他刚刚好')
+var cd='345'
+var cd1='34'
+var cd2='35'
+var cd3='5'
+var cd4='34rr5'
+console.log(cd4)
+console.log(cd2)
+var ws = new WebSocket("wss://echo.websocket.org");
+
+ws.onopen = function (evt) {
+    console.log("Connection open ...");
+    ws.send("Hello WebSockets!");
+};
+
+ws.onmessage = function (evt) {
+    console.log("Received Message: " + evt.data);
+    ws.close();
+};
+
+ws.onclose = function (evt) {
+    console.log("Connection closed.");
+};
