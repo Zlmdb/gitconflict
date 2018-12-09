ac0abaefc260f24ff33da01e3d512853c87fb852
Tony <htcm8w@163.com>
2018-12-09 15:59:52 +0800

    normal ci

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "browser": true,
+        "es6":true,
+        
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 5
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console":0
+    }
+};
--- dev.js
+++ dev.js
@@ -1,34 +1,68 @@
-console.log('dv发给他刚刚好')
-var cd='345'
-var cd1='34'
-var cd2='35'
-var cd3='5'
-var cd4='34rr5'
-console.log(cd4)
-console.log(cd2)
+// /* eslint-disable no-console*/
+console.log ("dv发给他刚刚好");
+var cd = "345";
+var cd1=cd;
+var cd2="35";
+var cd3="5";
+var cd4="34rr5";
+console.log(cd4);
+console.log(cd2);
 var ws = new WebSocket("wss://echo.websocket.org");
 
 ws.onopen = function (evt) {
-    console.log("Connection open ...");
-    ws.send("Hello WebSockets!");
+	console.log("Connection open ...");
+	ws.send("Hello WebSockets!");
 };
 
 ws.onmessage = function (evt) {
-    console.log("Received Message: " + evt.data);
-    ws.close();
+	console.log("Received Message: " + evt.data);
+	ws.close();
 };
 
 ws.onclose = function (evt) {
-    console.log("Connection closed.");
+	console.log("Connection closed.");
 };
-console.log(1)
+console.log(1);
 var count=0;
 while(true){
-    if (count>6){
-            break;
-    }
-    console.log('你好');
-    count++;
-}console.log(当前是在dev2分支上做提交)
-}console.log('nomo')
-在nomo分支再提交一次
+	if (count>6){
+		break;
+	}
+	console.log("你好");
+	count++;
+}
+console.log(当前是在dev2分支上做提交);
+console.log("nomo");
+//在nomo分支再提交一次
+function sum (x, y) {
+	return x + y;
+}
+sum(2,5);
+// class dfd {/*eslint-disabled-line */
+//     findIn () {
+//         console.log(9)
+//     }
+//     fn () {
+//         console.log(9)
+//     }
+// }
+//                 function di(){
+//                     console.log(4)
+//                 }
+
+// var store = new Vuex.Store({
+//     state: {
+//         count: 0
+//     },
+//     mutations: {
+//         increment:(state) => {
+//             const obj = state
+//             obj.count += 1
+//         },
+//         decrement: (state) => {
+//             const obj = state
+//             obj.count -= 1
+//         }
+//     }
+// })
+
